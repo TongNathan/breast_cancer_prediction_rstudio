3a5397ba11361117e86b0ee72e41347ea1a90465
Nathan Tong <102090809@student.swin.edu.au>
2020-09-09 14:41:35 +1000

    Creating Project

---
+++ cleaning_data.R
@@ -0,0 +1,21 @@
+#Installing packages
+library(tidyverse)
+
+#Reading data
+breast_cancer_data <- read.csv(file = 'breast-cancer.csv')
+
+#Summary of data
+summary(breast_cancer_data)
+
+#Replace '?' with NA
+breast_cancer_data[breast_cancer_data == '?'] <- NA
+
+#Checking for missing values
+colSums(is.na(breast_cancer_data))
+
+#Remove all columns with NA
+breast_cancer_data <- na.omit(breast_cancer_data) 
+
+#Add names to the different columns in the data.frame
+colnames(breast_cancer_data) <- c("class","age","menopause","tumor_size","inv_nodes","node_caps","deg_malig","breast","breast_quad","irradiat")
+
